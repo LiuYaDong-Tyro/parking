1989e14f7a61ba4cff0f8dfe9f6887781f81f7dd
lei.zhu1 <574805595@qq.com>
2022-03-21 16:31:36 +0800

    [init] create db

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,18 +1,21 @@
 provider "aws" {
-#  access_key = "${var.access_key}"
-#  secret_key = "${var.secret_key}"
-  region = "south-east-2"
+  region  = "ap-southeast-2"
+  profile = "tw-aws-beach"
 }
 
 resource "aws_dynamodb_table" "my_first_table" {
-  name        = var.table_name
+  name         = var.table_name
   billing_mode = var.table_billing_mode
-  hash_key       = "employee-id"
+  hash_key     = "employee-id"
   attribute {
     name = "employee-id"
     type = "S"
   }
   tags = {
-    environment       = var.environment
+    environment = var.environment
   }
 }
+
+
+
+
