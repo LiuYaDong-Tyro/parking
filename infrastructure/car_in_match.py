6b431f3a13c96ab6100788a51455470a47a01713
lei.zhu1 <574805595@qq.com>
2022-03-22 16:00:01 +0800

    [db] add lambda for car_in_function

---
+++ infrastructure/car_in_match.py
@@ -0,0 +1,15 @@
+import time
+
+
+def match(event, context):
+    print("Lambda function ARN:", context.invoked_function_arn)
+    print("CloudWatch log stream name:", context.log_stream_name)
+    print("CloudWatch log group name:",  context.log_group_name)
+    print("Lambda Request ID:", context.aws_request_id)
+    print("Lambda function memory limits in MB:", context.memory_limit_in_mb)
+    time.sleep(1)
+    print("Lambda time remaining in MS:", context.get_remaining_time_in_millis())
+
+    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])
+
+    return "haha, " + message
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -29,9 +29,11 @@ resource "aws_iot_topic_rule" "car_in_rule" {
   enabled     = true
   sql         = "SELECT * FROM 'topic/car' where state = 'car_in'"
   sql_version = "2016-03-23"
+
+  lambda {
+    function_arn = aws_lambda_function.cat_in_function.arn
+  }
 }
-
-
 
 resource "aws_iot_topic_rule" "car_out_rule" {
   name        = "car_out"
@@ -39,5 +41,40 @@ resource "aws_iot_topic_rule" "car_out_rule" {
   enabled     = true
   sql         = "SELECT * FROM 'topic/car' where state = 'car_out'"
   sql_version = "2016-03-23"
+}
+
+resource "aws_lambda_function" "cat_in_function" {
+  filename = "car_in_match.zip"
+  function_name = "match"
+  handler = "car_in_match.match"
+  role = aws_iam_role.iam_for_lambda.arn
+  source_code_hash = data.archive_file.car_in_function_source.output_base64sha256
+  runtime       = "python3.8"
+}
+
+data "archive_file" "car_in_function_source" {
+  type        = "zip"
+  source_file = "./car_in_match.py"
+  output_path = "car_in_match.zip"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
